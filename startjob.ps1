c4af501dbfc53f18f05a153541c6ff8449789345
aloyous <35898246+aloyouslio@users.noreply.github.com>
2018-11-14 10:29:57 +0800

    Create startjob.ps1

---
+++ startjob.ps1
@@ -0,0 +1,11 @@
+asnp VeeamPSSnapin
+$remote=Get-VBRJob  -Name "HQ_Remote" | where {$_.GetLastState() -ne "stopped"}
+$local1=Get-VBRJob  -Name "HQ_Local_1" | where {$_.GetLastState() -ne "stopped"}
+$local2=Get-VBRJob  -Name "HQ_Local_2" | where {$_.GetLastState() -ne "stopped"}
+if ($remote -Or $local1 -Or $local2){
+    write-host "Backup Job detected, terminating"
+}
+else{
+    write-host "Local backup will start...."
+    Get-VBRJob -Name "HQ_local_1" | Start-VBRJob -StartChainedJobs -RunAsync
+}
